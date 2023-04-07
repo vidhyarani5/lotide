9282f9200376958485815d01c3780a631651db56
Vidhyarani pinumalla <vidhyarani.pinumalla@hotmail.com>
2023-04-06 23:26:22 -0400

    Implement letterPositions

--- letterPositions.js
+++ letterPositions.js
@@ -1,5 +1,28 @@
 const letterPositions = function(sentence) {
   const results = {};
-  // logic to update results here
+  for (let i = 0; i < sentence.length; i++) {
+    if (sentence[i] !== " ") {
+      if (results[sentence[i]]) {
+        results[sentence[i]].push(i);
+      }  else {
+        results[sentence[i]] = [i];
+      }
+    }
+  }
   return results;
 };
+
+function assertArraysEqual(actual, expected) {
+  var index = 0;
+  for (var key in actual) {
+    if (actual[key] !== expected[key]) {
+      index++;
+  }}  
+  if (index === 0) {
+    console.log('Assertion Passed --> Expected "' + expected + '", and send "' + actual + '"');
+  } else {
+    console.log('Assertion Failed --> Expected "' + expected + '", but send "' + actual + '"');
+  }};
+
+console.log(letterPositions("lighthouse in the house"));
+assertArraysEqual(letterPositions("hello").e, [1]);
