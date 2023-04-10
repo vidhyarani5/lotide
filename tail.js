17e2aa0649163f988f43e1b43d7a4e5e182f2af0
Vidhyarani pinumalla <vidhyarani.pinumalla@hotmail.com>
2023-04-10 14:26:34 -0400

    added the test cases

--- tail.js
+++ tail.js
@@ -1,21 +1,8 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
- 
-  if( actual.toString() === expected.toString())
-    return "Assertion Passed: ["+actual+"] === ["+expected+"]"
-  else 
-    return "Assertion failed: ["+actual+"] !== ["+expected+"]"
-  
-};
-
 const tail = function(arr1) {
   let arr2 = [];
   for(let i=1; i<arr1.length; i++)
     arr2.push(arr1[i]);
   return arr2;
-};
-// TEST CODE
+}
 
-console.log(assertEqual(tail([5,6,7]), [6,7]));
-
-console.log(assertEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]));
+module.exports = tail;
