b0ffdbffd672d7e96ef74bcecfcc3418b0545851
Vidhyarani pinumalla <vidhyarani.pinumalla@hotmail.com>
2023-04-10 16:15:21 -0400

    added the test cases

--- eqArrays.js
+++ eqArrays.js
@@ -11,19 +11,4 @@ const eqArrays = function (firstArray,secondArray)
   return result;
 };
 
-const assertEqual = function(actual, expected) {
- 
-  if( actual === expected)
-    console.log("Assertion Passed: ["+actual+"] === ["+expected+"]");
-  else 
-    console.log("Assertion Failed: ["+actual+"] !== ["+expected+"]");
-  
-};
-
-console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
-
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])) // => false
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]));
+module.exports = eqArrays;
