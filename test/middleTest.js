5166ac9d4a03b31cea416680719302a1705de8c5
Vidhyarani pinumalla <vidhyarani.pinumalla@hotmail.com>
2023-04-10 21:37:43 -0400

    added mocha chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,6 +1,13 @@
-const middle = require('../middle');
-const assertEqual= require('../assertEqual');
 
-assertEqual(middle([1, 2, 3]),2);
-assertEqual(middle([1, 2, 3, 4, 5,6]), '3,4');
-assertEqual(middle([1, 2, 3, 4]), '2,3');
+
+const assert = require('chai').assert;
+const middle   = require('../middle');
+
+describe("#middle", () => {
+  it("returns 2 for [1, 2, 3]", () => {
+    assert.strictEqual(middle([1, 2, 3]), 2);
+  });
+  it("returns '3,4' for [1,2,3,4,5,6]", () => {
+    assert.strictEqual(middle([1,2,3,4,5,6]), '3,4'); 
+  });
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,5 +1,15 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
+const expect = require('chai').expect;
 const tail = require('../tail');
 
-assertEqual(tail([5,6,7]), [6,7]);
-assertEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+describe("#tail", () => {
+  it("returns [2,3] for [1, 2, 3]])", () => {
+    expect(tail([1, 2, 3])).to.eql([2, 3]);
+  });
+  it("returns ['Lighthouse', 'Labs'] for ['Hello', 'Lighthouse', 'Labs']", () => {
+    expect(tail(['Hello', 'Lighthouse', 'Labs'])).to.eql(['Lighthouse', 'Labs']); 
+  });
+  it("returns [6,7] for [5,6,7]", () => {
+    assert.deepEqual(tail([5,6,7]), [6,7]); 
+  });
+});
